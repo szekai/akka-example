f663caab6c8f157287adf322229418a5532545d3
szekai <szekai@Jaeger>
2014-04-28 11:32:58 +0800

    add TypedActor example 4

---
+++ TypedActorExample/src/main/scala/sky/calculator/example4/CalculatorActorSystem.scala
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package sky.calculator.example4
+
+import sky.calculator._
+import akka.actor.ActorSystem
+import akka.actor.TypedActor
+import akka.actor.TypedProps
+import akka.actor.ActorRef
+import com.typesafe.config.ConfigFactory
+import scala.concurrent.Await
+import scala.concurrent.duration._
+
+object CalculatorActorSystem {
+  def main(args: Array[String]): Unit = {
+
+    val _system = ActorSystem("TypedActorsExample", ConfigFactory.load().getConfig("TypedActorExample"))
+
+    val calculator: CalculatorInt =
+      TypedActor(_system).typedActorOf(TypedProps[Calculator]().withDispatcher("defaultDispatcher"))
+
+    calculator.incrementCount()
+
+    // Invoke the method and wait for result
+    val future = calculator.add(14, 6)
+    var result = Await.result(future, 5 second)
+    println("Result is " + result)
+
+    // Invoke the method and wait for result
+    var counterResult = calculator.incrementAndReturn()
+    println("Result is " + counterResult.get)
+
+    counterResult = calculator.incrementAndReturn()
+    println("Result is " + counterResult.get)
+
+    // Get access to the ActorRef
+    val calActor: ActorRef = TypedActor(_system).getActorRefFor(calculator)
+    // call actor with a message
+    calActor ! "Hi there"
+
+    _system.shutdown()
+  }
+}
